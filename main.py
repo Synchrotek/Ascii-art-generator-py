96c30a5c075eac0bbcce14fd803a3ca3ed3d5f2a
Satyajit Nayak <77431114+Synchrotek@users.noreply.github.com>
2023-10-14 10:22:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from PIL import Image 
+from PIL import Image
 
 '''change these values according to your needs'''
 image_path = "Imgs/dreamCar.jpg"  # Relative path of input image
